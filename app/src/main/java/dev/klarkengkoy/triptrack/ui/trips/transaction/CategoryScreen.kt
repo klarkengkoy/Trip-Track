0a118ff8da84be700c30362e26147d480e07b82b
klarkengkoy <48973950+klarkengkoy@users.noreply.github.com>
2025-11-13 23:00:17 +0900

    optimized imports

--- app/src/main/java/dev/klarkengkoy/triptrack/ui/trips/transaction/CategoryScreen.kt
+++ app/src/main/java/dev/klarkengkoy/triptrack/ui/trips/transaction/CategoryScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
